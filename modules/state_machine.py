6818a809e0c342a96bb9001f3e3c94b9a74cace2
jason <rodriguezjasonlloyd@gmail.com>
2025-10-15 05:59:49 +0800

    feat: add state machine

---
+++ modules/state_machine.py
@@ -0,0 +1,34 @@
+from enum import Enum, auto
+
+
+class State(Enum):
+    MainMenu = auto()
+    Quit = auto()
+
+    AnalyzeMenu = auto()
+    AnalyzeDescriptive = auto()
+    AnalyzeSampleBatch = auto()
+    AnalyzeSampleBatchNormalized = auto()
+
+    ExperimentMenu = auto()
+    ExperimentAll = auto()
+    ExperimentSelected = auto()
+
+
+class StateMachine:
+    def __init__(
+        self, initial_state: State, transitions: dict[State, list[State]]
+    ) -> None:
+        self.current_state: State = initial_state
+        self.transitions: dict[State, list[State]] = transitions
+
+    def transition(self, new_state: State) -> None:
+        if new_state in self.transitions[self.current_state]:
+            self.current_state = new_state
+        else:
+            raise ValueError(
+                f"Invalid transition from {self.current_state} to {new_state}"
+            )
+
+    def get_state(self) -> State:
+        return self.current_state
